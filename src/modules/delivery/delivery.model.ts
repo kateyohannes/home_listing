7381829036ab4dfb98e3eba3652fb41b3d50ba47
Yohannes Teshome <katteyohannes@gmail.com>
2023-09-13 10:27:52 -0400

    deleivery model

---
+++ src/modules/delivery/delivery.model.ts
@@ -0,0 +1,36 @@
+
+import { Schema, model } from "mongoose";
+
+enum Status {
+    Cancled = 'Cancled',
+    Inprogress = 'Inprogress',
+    Completed = 'Completed'
+}
+
+const DeliverySchema = new Schema({
+    user : {
+        type : Schema.Types.ObjectId,
+        ref : "user",
+        required : true
+    },
+    order : [{
+        type : Schema.Types.ObjectId,
+        ref : "order",
+        required : true
+    }],
+    status : {
+        type : String,
+        enum : Status,
+        default : Status.Inprogress
+    },
+    recivedAt : {
+        type : Date,
+        default : Date.now
+    },
+    deliveredAt : {
+        type : Date,
+        required : true
+    }
+})
+
+module.exports = model("delivery", DeliverySchema)
