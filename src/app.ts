188451ab130896c9ac0e2777729489468c8c42e1
Yohannes Teshome <katteyohannes@gmail.com>
2023-09-20 01:47:00 -0400

    app commit

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,37 @@
 
-import fastify, { FastifyInstance, FastifyRequest, FastifyReply } from "fastify";
-import mongoose from "mongoose";
+import multer from "fastify-multer";
+import fastifyCors from "@fastify/cors";
+import fastify, { FastifyInstance, FastifyRequest } from "fastify";
 
-mongoose.connect("mongodb://localhost/test_mongo").then(()=>{
-    console.log("mongodb is on ...");
-}).catch((err)=>{
-    console.log(err);
-});
+import db from "./plugin/mongoose";
+import brandRoute from "./router/Brand";
+import { brandSchema } from "./schema/Brand";
+
 
 export const server : FastifyInstance = fastify({
     logger : true
 });
 
-const port = 3000;
-const secret = "sduf09adsufa0sd9fasd09fu";
+const uri = "mongodb://localhost/test_mongo";
+const port = 3456;
+const parser = multer({
+    dest : "upload/"
+});
+
+server.decorate('multer', { parser });
+
+server.register(multer.contentParser);
+server.register(db, { uri });
 
 async function main(){
+    for(const schema of [...brandSchema]){
+        server.addSchema(schema);
+    }
+
     try{
+
+        server.register(brandRoute, { prefix : "/brand" });
+
         await server.listen({
             port,
             host : "0.0.0.0"
