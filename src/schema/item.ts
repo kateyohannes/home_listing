8874b94824df94cb7bab8b095201be93f95fa031
Yohannes Teshome <katteyohannes@gmail.com>
2023-09-25 17:45:55 -0400

    make itemImage optional, add array to itemCatagory and itemSubcatagory

--- src/schema/item.ts
+++ src/schema/item.ts
@@ -4,25 +4,26 @@ import { buildJsonSchemas } from "fastify-zod";
 const ItemForm = z.object({
     itemBrand : z.string(),
     itemName : z.string(),
-    itemImage : z.string(),
-    itemCatagory : z.string(),
-    itemSubCatagory : z.string().optional(),
+    itemImage : z.string().optional(),
+    itemCatagory : z.array(z.string()),
+    itemSubCatagory : z.array(z.string()),
     usage : z.array(z.string())
 })
 
 const ItemResponse = z.object({
+    _id: z.string(),
     itemBrand : z.string(),
     itemName : z.string(),
-    itemImage : z.string(),
-    itemCatagory : z.string(),
-    itemSubCatagory : z.string().optional(),
+    itemImage : z.string().optional(),
+    itemCatagory : z.array(z.string()),
+    itemSubCatagory : z.array(z.string()),
     usage : z.array(z.string())
-});
+}).optional();
 
 const ItemResponses = z.array(ItemResponse);
 
-export type InputCreateForm = z.infer<typeof ItemForm>;
-export type InputUpdateForm = z.infer<typeof ItemForm>;
+export type ItemCreateForm = z.infer<typeof ItemForm>;
+export type ItemUpdateForm = z.infer<typeof ItemForm>;
 
 export const { schemas : itemSchema, $ref } = buildJsonSchemas({
     ItemForm,
