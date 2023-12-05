c7afb18a90e7485a14c0f662b02dd1f9f1f2d9d6
Yohannes Teshome <katteyohannes@gmail.com>
2023-12-05 03:06:24 -0500

    add mongodb connection to hono

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,29 @@
-import { Hono } from 'hono'
+import { Context, Hono } from 'hono'
+import { cors } from 'hono/cors'
+import { logger } from 'hono/logger'
+import mongo from './config/mongo'
+import { Db } from 'mongodb'
 
-const app = new Hono()
+const app : Hono = new Hono()
 
-app.get('/', (c) => c.text('Hello Hono!'))
+app.use(cors())
+app.use("*", logger())
 
-export default app
+app.get('/', async (c : Context) => {
+    const db : Db = mongo.getDb();
+    const data = await db.collection("user").find().toArray();
+    return c.json(data, 200)
+})
+
+export default {
+    port : Number(Bun.env.PORT) || 3000,
+    fetch : app.fetch,
+    mongo : mongo.connect().then((err)=>{
+        if(err){
+            console.log("Error Occured: ", err)
+            return;
+        }
+        console.log("MongoDB is connected ...")
+    })
+
+}
