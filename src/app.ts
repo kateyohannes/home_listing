6e62a3d24fe4728062df5aa99dc833e596905e88
Yohannes Teshome <katteyohannes@gmail.com>
2023-12-06 12:09:31 -0500

    add catagory and brand

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,44 @@
 import { Context, Hono } from 'hono'
 import { cors } from 'hono/cors'
 import { logger } from 'hono/logger'
+
 import mongo from './config/mongo'
-import { Db } from 'mongodb'
+
+import item from './routes/item.route'
+import brand from './routes/brand.route'
+import catagory from './routes/catagory.route'
+
+import { errorHandler, notFound } from './middlewares/error.middleware'
 
 const app : Hono = new Hono()
 
 app.use(cors())
-app.use("*", logger())
+app.use('*', logger())
 
-app.get('/', async (c : Context) => {
-    const db : Db = mongo.getDb();
-    const data = await db.collection("user").find().toArray();
-    return c.json(data, 200)
+app.route('/item', item)
+app.route('/brand', brand)
+app.route('/catagory', catagory)
+
+app.onError((err : Error, c : Context)=>{
+    const error = errorHandler(c)
+    return error
 })
+
+app.notFound((c : Context)=>{
+    const error = notFound(c)
+    return error
+})
+
 
 export default {
     port : Number(Bun.env.PORT) || 3000,
     fetch : app.fetch,
     mongo : mongo.connect().then((err)=>{
         if(err){
-            console.log("Error Occured: ", err)
+            console.log('Error Occured: ', err)
             return;
         }
-        console.log("MongoDB is connected ...")
+        console.log('MongoDB is connected ...')
     })
 
 }
