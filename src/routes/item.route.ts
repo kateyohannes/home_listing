1a86562cc5b4e89f4f939685d02bdc704e40ae4e
Yohannes Teshome <katteyohannes@gmail.com>
2023-12-08 12:20:34 -0500

    quantity from items_detail quantity

---
+++ src/routes/item.route.ts
@@ -0,0 +1,78 @@
+
+import { Hono, Context } from "hono";
+import { Db, ObjectId } from "mongodb";
+import { zValidator } from "@hono/zod-validator";
+
+import mongo from "../config/mongo";
+import { itemSchema } from "../schemas/item.schema";
+
+const route : Hono = new Hono();
+
+route.get("/", async (c : Context)=>{
+    const db : Db = mongo.getDb()
+    const data = await db.collection("item").find().toArray()
+    return c.json(data, 200)
+})
+
+route.get("/:_id", async (c : Context)=>{
+    const db : Db = mongo.getDb()
+    const { _id } = c.req.param()
+    const data = await db.collection("item").findOne({ _id : new ObjectId(_id)})
+    return c.json(data, 200)
+})
+
+route.post("/add", 
+    zValidator("json", itemSchema, (result, c : Context)=>{
+        if(!result.success){
+            throw new Error(`Invalid Input!, ${result.error}`)
+        }
+    }), 
+    async (c : any)=>{
+        const db : Db = mongo.getDb()
+        const body = await c.req.valid("json")
+        const data = {
+
+        }
+        const doc = await db.collection("item").insertOne(body)
+        return c.json(doc, 201)
+    }
+)
+
+route.post("/add/:_id", async(c : Context)=>{
+    const db : Db = mongo.getDb()
+    const { _id } = c.req.param()
+    const body = await c.req.json()
+    const doc : {} = await db.collection("item").updateOne({
+        _id : new ObjectId(_id)
+    },{
+        $push : {
+            item_details : {
+                _id: ObjectId.generate(),           
+                ...body
+            }
+        },
+        $inc : {
+            quantity : body.quantity
+        }
+    })
+
+    return c.json(doc, 201)
+
+})
+
+route.delete("/delete/:_id", async(c : Context)=>{
+    const db : Db = mongo.getDb()
+    const { _id } = c.req.param()
+    const doc = await db.collection("item").deleteOne({ _id : new ObjectId(_id)})
+    return c.json(doc, 204)
+})
+
+route.delete("/delete_all", async (c : Context)=>{
+    const db : Db = mongo.getDb();
+    const doc = await db.collection("item").deleteMany()
+    return c.json(doc, 204)
+
+})
+
+
+export default route
