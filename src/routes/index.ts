f9faf50a27f4b8e64e6ab9fcbf16cca193ddc0b5
Yohannes Teshome <katteyohannes@gmail.com>
2023-12-19 13:22:24 -0500

    route index

---
+++ src/routes/index.ts
@@ -0,0 +1,7 @@
+
+import item from '@route/item.route'
+import cart from '@route/cart.route'
+import brand from '@route/brand.route'
+import catagory from '@route/catagory.route'
+
+export { item, cart, brand, catagory }
