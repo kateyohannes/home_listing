a66869b4f54e823f125c38fdbd0e85a95c7ac2be
Yohannes Teshome <katteyohannes@gmail.com>
2024-01-09 10:31:17 -0500

    remove cart from import index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,7 @@
 
 import item from '@route/item.route'
-import cart from '@route/cart.route'
 import brand from '@route/brand.route'
 import catagory from '@route/catagory.route'
 import order from '@route/order.route'
 
-export { item, cart, brand, catagory, order }
+export { item, brand, catagory, order }
